123539cbb3ef07c73939e271d4f61db5f85db50c
Tim Langford <tim.langford@gmail.com>
2019-05-15 21:07:11 +0100

    Updated tests. Test for used ident.

--- test/e2e/memoria/api/identity_test.go
+++ test/e2e/memoria/api/identity_test.go
@@ -3,7 +3,6 @@ package identity_test
 import (
 	"bytes"
 	"encoding/json"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -11,10 +10,15 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
-	"github.com/templecloud/memoria-server/test/e2e/memoria/framework"
 	"github.com/templecloud/memoria-server/internal/memoria/boot"
 	"github.com/templecloud/memoria-server/internal/memoria/controller/identity"
+	"github.com/templecloud/memoria-server/test/e2e/memoria/framework"
 )
+
+// curl -v localhost:8080/api/v1/health
+// curl -v -X POST localhost:8080/api/v1/signup -d '{ "name": "test-user", "email": "test@test.com", "password": "test" }
+// curl -v localhost:8080/api/v1/login -d '{ "email": "test@test.com", "password": "test" }'
+// curl -v --cookie "token=${JWT}" localhost:8080/api/v1/health
 
 func TestIdentity(t *testing.T) {
 	RegisterFailHandler(Fail)          // Register `gomega` with `ginkgo`.
@@ -40,7 +44,6 @@ var _ = Describe("Identity", func() {
 
 			It("should not be possible to get the health of the server", func() {
 				router := boot.NewServer()
-				// curl -v localhost:8080/api/v1/health
 				actual := invoke(router, "GET", "/api/v1/health", nil, nil)
 				Expect(actual).NotTo(BeNil())
 				Expect(actual.Code).To(Equal(http.StatusUnauthorized))
@@ -54,39 +57,61 @@ var _ = Describe("Identity", func() {
 				login := identity.Login{Email: "test@test.com", Password: "test"}
 				signup := identity.Signup{Name: "Test", Login: login}
 
-				// curl -v -X POST localhost:8080/api/v1/signup -d '{ "name": "test-user", "email": "test@test.com", "password": "test" }
-				body, _ := json.Marshal(signup)
-				reader := bytes.NewReader(body)
-				actual := invoke(router, "POST", "/api/v1/signup", reader, nil)
+				actual := invoke(router, "POST", "/api/v1/signup", signup, nil)
 				Expect(actual).NotTo(BeNil())
 				Expect(actual.Code).To(Equal(http.StatusOK))
 
-				// curl -v localhost:8080/api/v1/login -d '{ "email": "test@test.com", "password": "test" }'
-				body, _ = json.Marshal(login)
-				reader = bytes.NewReader(body)
-				actual = invoke(router, "POST", "/api/v1/login", reader, nil)
+				actual = invoke(router, "POST", "/api/v1/login", login, nil)
 				Expect(actual).NotTo(BeNil())
 				Expect(actual.Code).To(Equal(http.StatusOK))
 				cookies := actual.Result().Cookies()
 				Expect(cookies).NotTo(BeNil())
 
-				// curl -v --cookie "token=${JWT}" localhost:8080/api/v1/health
 				actual = invoke(router, "GET", "/api/v1/health", nil, cookies)
 				Expect(actual).NotTo(BeNil())
 				Expect(actual.Code).To(Equal(http.StatusOK))
 			})
 		})
 	})
+
+	Describe("Checking user creation", func() {
+
+		Context("When not logged in", func() {
+
+			login := identity.Login{Email: "test2@test.com", Password: "test2"}
+			signup := identity.Signup{Name: "Test2", Login: login}
+
+			It("should be possible to create a new user", func() {
+				router := boot.NewServer()
+
+				actual := invoke(router, "POST", "/api/v1/signup", signup, nil)
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Code).To(Equal(http.StatusOK))
+			})
+
+			It("should not be possible to create a new user when the username is taken", func() {
+				router := boot.NewServer()
+
+				actual := invoke(router, "POST", "/api/v1/signup", signup, nil)
+				Expect(actual).NotTo(BeNil())
+				Expect(actual.Code).To(Equal(http.StatusConflict))
+				Expect(body(actual)["errorMessage"]).To(Equal("User already registered."))
+			})
+		})
+	})
+
 })
 
 // invoke the specified server endpoint and record the results.
 func invoke(
 	handler http.Handler, method,
 	path string,
-	body io.Reader,
+	data interface{},
 	cookies []*http.Cookie,
 ) *httptest.ResponseRecorder {
-	req, _ := http.NewRequest(method, path, body)
+	body, _ := json.Marshal(data)
+	reader := bytes.NewReader(body)
+	req, _ := http.NewRequest(method, path, reader)
 	if cookies != nil {
 		for _, cookie := range cookies {
 			req.AddCookie(cookie)
@@ -96,3 +121,13 @@ func invoke(
 	handler.ServeHTTP(recorder, req)
 	return recorder
 }
+
+func body(recorded *httptest.ResponseRecorder) map[string]interface{} {
+	var body map[string]interface{}
+	err := json.Unmarshal(recorded.Body.Bytes(), &body)
+	if err != nil {
+		panic(err)
+	}
+	return body
+}
+
