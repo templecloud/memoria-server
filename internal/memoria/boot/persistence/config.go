73c316b2ff75bd5ba4db1fed8f9ec4c05b2950a6
Tim Langford <tim.langford@gmail.com>
2019-05-26 19:57:18 +0100

    Added configuration and tests for the persistence package.

---
+++ internal/memoria/boot/persistence/config.go
@@ -0,0 +1,64 @@
+package persistence
+
+import (
+	"fmt"
+)
+
+const (
+	// Localhost is the localhost domain.
+	Localhost = "localhost"
+	// MongoDBProtocol is the MongoDB protocol.
+	MongoDBProtocol = "mongodb"
+	// MongoDBDefaultPort is the MongoDB default port.
+	MongoDBDefaultPort = 27017
+)
+
+//-------------------------------------------------------------------------------------------------
+// Models
+
+// Config denotes the configuration of the server controller.
+type Config struct {
+	Connection *ConnectionConfig `json:"connection" binding:"optional"`
+}
+
+// ConnectionConfig denotes the configuration of the datastore.
+type ConnectionConfig struct {
+	Protocol string `json:"protocol" binding:"required"` 
+	Host  string `json:"host" binding:"required"`
+	Port int    `json:"port" binding:"required"`
+}
+
+//-------------------------------------------------------------------------------------------------
+// Private
+
+// Return a default MongoDB connection: "mongodb://localhost:27017"
+func newDefaultMongoDBConfig() *Config {
+	return &Config{
+		Connection: &ConnectionConfig{
+			Protocol: MongoDBProtocol,
+			Host:  Localhost,
+			Port: MongoDBDefaultPort,
+		},
+	}
+}
+
+// Get the datasource connection URL. e.g. "mongodb://localhost:27017"
+func (c *Config) url() string {
+	c.validate()
+	return fmt.Sprintf("%s://%s:%d", c.Connection.Protocol, c.Connection.Host, c.Connection.Port)
+}
+
+func (c *Config) validate() {
+	if c.Connection == nil {
+		panic(fmt.Sprintf("The datasource connection was not defined."))
+	}	
+	if c.Connection.Protocol == "" {
+		panic(fmt.Sprintf("The datasource connection protocol was not defined."))
+	}
+	if c.Connection.Host == "" {
+		panic(fmt.Sprintf("The datasource connection host was not defined."))
+	}
+	if c.Connection.Port < 1000 {
+		panic(fmt.Sprintf("The datasource connection port '%d' was less than 1000.", c.Connection.Port))
+	}
+}
---
+++ internal/memoria/boot/persistence/config_test.go
@@ -0,0 +1,85 @@
+package persistence
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+// TestMongoDBConfigURL ensure the 'happy path' works as expected when generating a URL.
+func TestMongoDBConfigURL(t *testing.T) {
+	// defaultConfig
+	defaultMongoDBConfig := newDefaultMongoDBConfig()
+	// Config01
+	mongoDBConfig01 := newDefaultMongoDBConfig()
+	somehost01 := "somehost"
+	mongoDBConfig01.Connection.Host = somehost01
+	// Config02
+	mongoDBConfig02 := newDefaultMongoDBConfig()
+	somePort02 := 27018
+	mongoDBConfig02.Connection.Port = somePort02
+	// Config03
+	mongoDBConfig03 := newDefaultMongoDBConfig()
+	mongoDBConfig03.Connection.Host = somehost01
+	mongoDBConfig03.Connection.Port = somePort02
+	// Tests.
+	var tests = []struct {
+		config   *Config
+		expected string
+	}{
+		{defaultMongoDBConfig, fmt.Sprintf("%s://%s:%d", MongoDBProtocol, Localhost, MongoDBDefaultPort)},
+		{mongoDBConfig01, fmt.Sprintf("%s://%s:%d", MongoDBProtocol, somehost01, MongoDBDefaultPort)},
+		{mongoDBConfig02, fmt.Sprintf("%s://%s:%d", MongoDBProtocol, Localhost, somePort02)},
+		{mongoDBConfig03, fmt.Sprintf("%s://%s:%d", MongoDBProtocol, somehost01, somePort02)},
+	}
+	// Run tests.
+	for _, test := range tests {
+		assert.Equal(t, test.expected, test.config.url())
+	}
+}
+
+// TestConnectionConfigValidation test the config will correctly panic if validation fails.
+func TestConnectionConfigValidation(t *testing.T) {
+	// Tests.
+	var tests = []struct {
+		config   *Config
+		expected string
+	}{
+		{	&Config{}, 
+			"The datasource connection was not defined.",
+		},
+		{
+			&Config{
+				Connection: &ConnectionConfig{}, 
+			},
+			"The datasource connection protocol was not defined.",
+		},
+		{
+			&Config{
+				Connection: &ConnectionConfig{Protocol: MongoDBProtocol}, 
+			},
+			"The datasource connection host was not defined.",
+		},
+		{
+			&Config{
+				Connection: &ConnectionConfig{Protocol: MongoDBProtocol, Host: Localhost}, 
+			},
+			"The datasource connection port '0' was less than 1000.",
+		},
+		{
+			&Config{
+				Connection: &ConnectionConfig{Protocol: MongoDBProtocol, Host: Localhost, Port: -1}, 
+			},
+			"The datasource connection port '-1' was less than 1000.",
+		},		
+	}
+	// Run tests.
+	for _, test := range tests {
+		f := func() {
+    		test.config.validate()
+		}
+		assert.Panics(t, f)
+		assert.PanicsWithValue(t, test.expected, f)
+	}
+}
